64528965042933fed5ee58a6c3ca732e4a177076
Gaurav Kedia <gaurav.kedia18@Gmail.com>
2024-02-11 23:24:52 +0530

    Update Deploy-Script.sh

--- Scripts/Deploy-Script.sh
+++ Scripts/Deploy-Script.sh
@@ -1,6 +1,9 @@
 echo "********** Running Deploy Script .sh file **********"
 cd
-cd /home/ubuntu/React-1/test-env-foreverjava-React/Scripts
+cd /home/ubuntu/React-1/test-env-foreverjava-React/
+npm run build
+sudo cp -r build/* /var/www/html/
 ls -l
 pwd
+sudo systemctl reload apache2
 echo "********** End of Deploy Script .sh file 1 **********"
