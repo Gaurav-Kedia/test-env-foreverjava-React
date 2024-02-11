186b65b4c9d78f3b27b23f0f2e18ce596f0bb81c
Gaurav kedia <Gaurav.kedia18@gmail.com>
2024-02-11 22:50:23 +0530

    added scripts for react app in test env

---
+++ Scripts/Deploy-Script.sh
@@ -0,0 +1,6 @@
+echo "********** Running Deploy Script .sh file **********"
+cd
+cd /home/ubuntu/React-1/test-env-foreverjava-React/Scripts
+ls -l
+pwd
+echo "********** End of Deploy Script .sh file 1 **********"
